69c2ca19884639105acc6cee38d584a0ce78bf16
NazarStf <hworker98@gmail.com>
2023-06-04 23:09:46 +0300

    Implementing Create Coffee Order Webservice Function

---
+++ CoffeeOrderingApp/Models/CreateOrderResponse.swift
@@ -0,0 +1,12 @@
+//
+//  CreateOrderResponse.swift
+//  CoffeeOrderingApp
+//
+//  Created by NazarStf on 04.06.2023.
+//
+
+import Foundation
+
+struct CreateOrderResponse: Decodable {
+	let success: Bool
+}
