013b274165718b319f929c4b35ae62c96b3f37fd
NazarStf <hworker98@gmail.com>
2023-06-05 00:10:42 +0300

    Adding Navigation from ContentView to AddCoffeeOrderView

--- CoffeeOrderingApp/Views/ContentView.swift
+++ CoffeeOrderingApp/Views/ContentView.swift
@@ -11,14 +11,39 @@ struct ContentView: View {
 	
 	@ObservedObject private var orderListVM = OrderListViewModel()
 	
+	@State private var showModal: Bool = false
+	
 	var body: some View {
 		
 		NavigationView {
-			OrderListView(orders: self.orderListVM.orders)
-				.navigationBarTitle("Coffee Orders")
-		}
+		
+		OrderListView(orders: self.orderListVM.orders)
 		 
+			.navigationBarTitle("Coffee Orders")
+			.navigationBarItems(leading: Button(action: reloadOrders) {
+				Image(systemName: "arrow.clockwise")
+					.foregroundColor(Color.white)
+				}, trailing: Button(action: showAddCoffeeOrderView){
+					Image(systemName: "plus")
+						.foregroundColor(Color.white)
+			})
+		  
+			
+			.sheet(isPresented: self.$showModal) {
+				AddCoffeeOrderView(isPresented: self.$showModal)
+			}
+			
+		}
 	}
+	
+	private func reloadOrders() {
+		self.orderListVM.fetchOrders()
+	}
+	
+	private func showAddCoffeeOrderView() {
+		self.showModal = true
+	}
+	
 
 }
 
