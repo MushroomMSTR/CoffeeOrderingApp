6720362f44bd24fe0b7a7b3311f50d4db2ad66b6
NazarStf <hworker98@gmail.com>
2023-06-04 22:31:49 +0300

    Creating Webservice

--- CoffeeOrderingApp/Services/Webservice.swift
+++ CoffeeOrderingApp/Services/Webservice.swift
@@ -8,5 +8,29 @@
 import Foundation
 
 class Webservice {
-	
+	func getAllOrders(completion: @escaping ([Order]?) -> ()) {
+		
+		guard let url = URL(string: "https://island-bramble.glitch.me/orders") else {
+			completion(nil)
+			return
+		}
+		
+		URLSession.shared.dataTask(with: url) { data, response, error in
+			
+			guard let data = data, error == nil else {
+				DispatchQueue.main.async {
+					completion(nil)
+				}
+				return
+			}
+			
+			let orders = try? JSONDecoder().decode([Order].self, from: data)
+			DispatchQueue.main.async {
+				completion(orders)
+			}
+			
+		}.resume()
+		
+		
+	}
 }
