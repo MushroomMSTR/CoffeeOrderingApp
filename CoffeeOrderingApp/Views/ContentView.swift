94ace85d672f27f905a3dd1f44c95de9c51ce877
NazarStf <hworker98@gmail.com>
2023-06-05 00:21:40 +0300

    Add UINavigationBar in to ContentView. Also add starbucksGreen color

--- CoffeeOrderingApp/Views/ContentView.swift
+++ CoffeeOrderingApp/Views/ContentView.swift
@@ -8,31 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-	
 	@ObservedObject private var orderListVM = OrderListViewModel()
-	
 	@State private var showModal: Bool = false
 	
+	init() {
+		configureNavigationBarAppearance()
+	}
+	
 	var body: some View {
-		
 		NavigationView {
-		
-		OrderListView(orders: self.orderListVM.orders)
-		 
-			.navigationBarTitle("Coffee Orders")
-			.navigationBarItems(leading: Button(action: reloadOrders) {
-				Image(systemName: "arrow.clockwise")
-					.foregroundColor(Color.white)
-				}, trailing: Button(action: showAddCoffeeOrderView){
-					Image(systemName: "plus")
-						.foregroundColor(Color.white)
-			})
-		  
-			
-			.sheet(isPresented: self.$showModal) {
-				AddCoffeeOrderView(isPresented: self.$showModal)
-			}
-			
+			OrderListView(orders: self.orderListVM.orders)
+				.navigationBarTitle("Coffee Orders")
+				.navigationBarItems(
+					leading: Button(action: reloadOrders) {
+						Image(systemName: "arrow.clockwise")
+							.foregroundColor(Color.white)
+					},
+					trailing: Button(action: showAddCoffeeOrderView) {
+						Image(systemName: "plus")
+							.foregroundColor(Color.white)
+					}
+				)
+				.sheet(isPresented: self.$showModal) {
+					AddCoffeeOrderView(isPresented: self.$showModal)
+				}
 		}
 	}
 	
@@ -44,11 +43,18 @@ struct ContentView: View {
 		self.showModal = true
 	}
 	
+	private func configureNavigationBarAppearance() {
+		let starbucksGreen = UIColor(red: 0/255, green: 112/255, blue: 74/255, alpha: 1.0)
+		
+		UINavigationBar.appearance().backgroundColor = starbucksGreen
+		UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+		UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+	}
 
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
