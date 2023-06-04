37c5a792f5f9b67c468c459cad210652777dd819
NazarStf <hworker98@gmail.com>
2023-06-04 22:13:42 +0300

    Initial Commit

---
+++ CoffeeOrderingApp/CoffeeOrderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderingAppApp.swift
+//  CoffeeOrderingApp
+//
+//  Created by NazarStf on 04.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
